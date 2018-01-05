388ddf1fb040a0ffae70143afc765dfe971c2bc3
spagabtti <spagabtti@gmail.com>
2018-01-04 16:21:33 -0800

    edit

---
+++ app/src/main/java/com/spaga/eggtimer/MainActivity.java
@@ -0,0 +1,40 @@
+package com.spaga.eggtimer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SeekBar timerSeekBar = findViewById(R.id.timerSeekBar);
+        final TextView timerTextView = findViewById(R.id.timerTextView);
+
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(300);
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int minutes = (int) (i / 60);
+                int seconds = i - minutes * 60;
+
+                timerTextView.setText(Integer.toString(minutes) + ":" + Integer.toString(seconds));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
